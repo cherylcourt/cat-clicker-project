d317f224bc6da4a3201efd027b821ceae252ee8d
Cheryl Court <cheryl.court@gmail.com>
2015-01-25 08:34:38 -0700

    Added display of cat name on cat clicker

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ CatClicker.prototype.getHtml = function() {
     return '<div style="position: relative; width: 100%">' +
         '<img id="' + this.imageID + '" src="images/' + this.image + '" width="500" height="375">' +
         '<h2 id="' + this.numberOfClicksID + '" style="position: absolute; top: 0px; left: 25px; width: 100%; color: '+ this.color +'">0</h2>' +
+        '<h1 style="position: absolute; bottom: 0px; left: 25px; width: 100%; color: '+ this.color +'">' + this.name + '</h1>' +
         '</div>';
 };
 
