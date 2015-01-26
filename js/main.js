3c3ca2bc4ae9d082a56517b1512c72857eb8f7d7
Cheryl Court <cheryl.court@gmail.com>
2015-01-25 18:58:19 -0700

    Added list of cat names, when clicking on shows the corresponding cat clicker

--- js/main.js
+++ js/main.js
@@ -7,13 +7,11 @@ var CatClicker = function(index, name) {
 };
 
 CatClicker.prototype.getHtml = function() {
-    var catImage = '<img id="' + this.imageID + '" src="http://lorempixel.com/500/375/cats/' + this.index + '" width="500" height="375">';
+    var catImage = '<img id="' + this.imageID + '" src="http://lorempixel.com/500/375/cats/' + this.index + '" width="500" height="375">',
+        clickNumberRendering = '<h2 style="position: absolute; top: 0px; left: 25px; width: 100%"><span id="' + this.numberOfClicksID + '" >' + this.numberOfClicks + '</span></h2>',
+        catNameRendering = '<h1 style="position: absolute; bottom: 0px; left: 25px; width: 100%"><span>' + this.name + '</span></h1>';
 
-    return '<div style="position: relative; width: 100%">' +
-        catImage +
-        '<h2 style="position: absolute; top: 0px; left: 25px; width: 100%"><span id="' + this.numberOfClicksID + '" >0</span></h2>' +
-        '<h1 style="position: absolute; bottom: 0px; left: 25px; width: 100%"><span>' + this.name + '</span></h1>' +
-        '</div>';
+    return catImage + clickNumberRendering + catNameRendering;
 };
 
 CatClicker.prototype.setupClickEvent = function() {
